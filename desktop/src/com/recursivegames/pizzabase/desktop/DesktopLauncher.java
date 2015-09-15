ac743e224903160a0a3b0319b2b0ed5fb5da5084
Pegasuraptor <jake.rowe@hotmail.co.uk>
2015-09-15 20:10:20 +0100

    Initial commit.

---
+++ desktop/src/com/recursivegames/pizzabase/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.recursivegames.pizzabase.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.recursivegames.pizzabase.PizzaBaseGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PizzaBaseGame(), config);
+	}
+}
